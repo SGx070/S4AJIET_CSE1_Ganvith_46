a865bf7e70f54dfbf996cb91309fac7cb459c407
Ganvith Shriyan <ganvithshriyan841@gmail.com>
2026-05-05 14:15:43 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int v = x;
+        int rev = 0;
+        if(x<0)
+            return false;
+        while(x!=0){
+            int d = x %10;
+            rev = (rev*10)+d;
+            x = x/10;
+        }return v == rev;
+    }
+}
