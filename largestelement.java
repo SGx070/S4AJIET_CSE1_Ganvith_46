e7eb1cbda1c3d98f92fdcd3a1ae8ded404666a82
Ganvith Shriyan <ganvithshriyan841@gmail.com>
2026-05-05 09:56:55 +0530

    Create largestelement.java

---
+++ largestelement.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+
+		int[] a = {10,20,30,40,50};
+		int p=a[0];
+		int n = a.length;
+		for(int i =0; i<n;i++){
+		    if(a[i] > p)
+		        p = a[i];
+		}
+		System.out.println("The largest element in array: "+p);
+	}
+}
